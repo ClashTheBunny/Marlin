ca9fa0cf59dd0d24852bc5a22a2b829b3ed5e04c
Randall Mason <randall@mason.ch>
2018-08-30 21:44:40 -0500

    Get closer with STEPS per UNIT, still about 15% off.
    Stutters at more actual values. Tried 3964, 4000.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -601,7 +601,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 1600, 180 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 3200, 180 }
 
 /**
  * Default Max Feed Rate (mm/s)
